c85d352afb0a9a0df72f0909779643b2269a4df8
Ryner Gabriel <drryner@icloud.com>
2019-09-14 12:34:28 +0300

    Initial commit

---
+++ default.nix
@@ -0,0 +1,26 @@
+(import ./reflex-platform {
+  config.android_sdk.accept_license = true;
+}).project ({ pkgs, ... }: {
+  packages = {
+    common = ./common;
+    backend = ./backend;
+    frontend = ./frontend;
+  };
+  
+  shells = {
+    ghc = ["common" "backend" "frontend"];
+    ghcjs = ["common" "frontend"];
+  };
+
+  android.frontend = {
+    executableName = "frontend";
+    applicationId = "org.example.frontend";
+    displayName = "Example Android App";
+  };
+
+  ios.frontend = {
+    executableName = "frontend";
+    bundleIdentifier = "org.example.frontend";
+    bundleName = "Example iOS App";
+  };
+})
